47ecf7c8cf11933ad8056efdd6782fcfb7280369
viberring <viberring@gmail.com>
2017-08-18 00:59:24 +0800

    tooltip jqury

---
+++ JqueryDemo/Shopping/scripts/input.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * Created by Administrator on 2017/8/17.
+ */
+
+$(function () {
+
+    $("#inputSearch").focus(function () {
+        $(this).addClass("focus");
+        if ($(this).val() == this.defaultValue){
+            $(this).val("");
+        }
+    }).blur(function () {
+        $(this).removeClass("focus");
+        if ($(this).val() == ''){
+            $(this).val(this.defaultValue);
+        }
+    }).keyup(function (e) {
+        if (e.which == 13){
+            alert("回车提交表单");
+        }
+    })
+});
---
+++ JqueryDemo/Shopping/scripts/tooltip.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Created by Administrator on 2017/8/18.
+ */
+
+$(function () {
+    var x = 10;
+    var y = 20;
+    $("a.tooltip").mouseover(function (e) {
+        this.myTitle = this.title;
+        this.title = "";
+        var tooltip
+
+
+    })
+
+});
